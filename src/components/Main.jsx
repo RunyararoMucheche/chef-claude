c6fd7b28d2b25f8dbafe14ac3b57e28276ef144c
RunyararoMucheche <your_email@example.com>
2026-02-25 10:02:58 +0200

    Add function to add to list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,30 @@
 export default function Main() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    ingredients.push(newIngredient);
+    console.log(ingredients);
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form onSubmit={handleSubmit} className="add-ingredient-form">
         <input
           aria-label="Add ingredient"
           type="text"
           placeholder="e.g oregano"
+          name="ingredient"
         />
         <button>+ Add Ingredient</button>
       </form>
+      <ul>{ingredientsListItems}</ul>
     </main>
   );
 }
