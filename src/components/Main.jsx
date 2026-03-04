778e019a95d9e9a36cd32e8af5fe482473d5c9dc
RunyararoMucheche <your_email@example.com>
2026-03-04 13:29:16 +0200

    Add more state functionality and conditional ternary rendering

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,30 +1,50 @@
+import { useState } from "react";
+
 export default function Main() {
-  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+  const [ingredients, setIngredients] = useState([
+    "all the main spices",
+    "pasta",
+    "ground beef",
+    "tomato paste",
+  ]);
 
   const ingredientsListItems = ingredients.map((ingredient) => (
     <li key={ingredient}>{ingredient}</li>
   ));
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
+  function addIngredient(formData) {
     const newIngredient = formData.get("ingredient");
-    ingredients.push(newIngredient);
-    console.log(ingredients);
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
   }
 
   return (
     <main>
-      <form onSubmit={handleSubmit} className="add-ingredient-form">
+      <form action={addIngredient} className="add-ingredient-form">
         <input
           aria-label="Add ingredient"
           type="text"
           placeholder="e.g oregano"
           name="ingredient"
         />
-        <button>+ Add Ingredient</button>
+        <button>Add Ingredient</button>
       </form>
-      <ul>{ingredientsListItems}</ul>
+      {ingredients.length > 0 && (
+        <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list" aria-live="polite">
+            {ingredientsListItems}
+          </ul>
+          {ingredients.length > 3 && (
+            <div className="get-recipe-container">
+              <div>
+                <h3>Ready for a recipe?</h3>
+                <p>Generate a recipe from your list of ingredients.</p>
+              </div>
+              <button>Get a recipe</button>
+            </div>
+          )}
+        </section>
+      )}
     </main>
   );
 }
